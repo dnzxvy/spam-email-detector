686ddab29901025c35db022b4d066f93c903f175
dnzxvy <ntukadaniel@gmail.com>
2026-08-07 13:56:45 +0100

    trained the dataset and have made predictions for the model

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import re
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import joblib
+from sklearn.metrics import accuracy_score
+
 
 # Downloading spam email classification
 path = kagglehub.dataset_download("ashfakyeafi/spam-email-classification")
@@ -375,9 +379,22 @@ tfidf = TfidfVectorizer(stop_words="english", max_features=5000)
 X =  tfidf.fit_transform(X)
 
 # Splitting the dataset
-X_train, X_test, y_train, y_test = train_test_split(test_size=0.2,
-                                                    random_state=42,
-                                                    stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y)
+
+
+# Training the model via naive bayes
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Generating predictions for the model
+prediction = model.predict(X_test)
+
+
 
 #spam_dataset.to_csv("SpamEmail Dataset V2", index=False)
 
